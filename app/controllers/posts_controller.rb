68c45af609485ad6188df0b81b47c663e4146403
kjdemo <69888834+kjdemo@users.noreply.github.com>
2020-09-02 14:28:37 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,28 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")#DESC は降順。昇順はABC
-    # すべてのレコードを@postsに代入
+    # postテーブル内のすべてのレコードを@postsに代入
   end
 
   def create
     Post.create(content: params[:detail])
     #detaol はビューファイindex.htmlに書いてあるtextfieldの後ろ。textフィールドに入力された値をdetailと紐付けてparamsで取得。
     #content: は、マイグレーションファイルで決めたカラム名のcontent。IDとかcontent とかcreated_atとかある内のcontent だけとってるという意味
+    #Post.createでPostテーブルにcontent だけ保存してる
     redirect_to action: :index
     redirect_to action: :index
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    #post(小文字の)に指定されたidの情報（レコード）を１つ（findだから)取って代入
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 end
