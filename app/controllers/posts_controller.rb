8d6a5cd57d4d185751d5c918081f6aa20fa39d05
kjdemo <69888834+kjdemo@users.noreply.github.com>
2020-09-02 11:26:54 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all  # すべてのレコードを@postsに代入
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC")#DESC は降順。昇順はABC
+    # すべてのレコードを@postsに代入
   end
 
   def create
-    Post.create(content: params[:detail])　
-    #detaol はビューファイルnew.htmlに書いてあるtextfieldの後ろ。textフィールドに入力された値をdetailと紐付けてparamsで取得。
+    Post.create(content: params[:detail])
+    #detaol はビューファイindex.htmlに書いてあるtextfieldの後ろ。textフィールドに入力された値をdetailと紐付けてparamsで取得。
     #content: は、マイグレーションファイルで決めたカラム名のcontent。IDとかcontent とかcreated_atとかある内のcontent だけとってるという意味
+    redirect_to action: :index
+    redirect_to action: :index
   end
 end
